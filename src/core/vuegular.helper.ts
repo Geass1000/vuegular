854f1dcd3b40e0b79118396b06cc587d2973e3ca
Andrey <geass1000@gmail.com>
2019-06-14 10:13:32 +0300

    add: `getElementConfig` method

--- src/core/vuegular.helper.ts
+++ src/core/vuegular.helper.ts
@@ -3,4 +3,8 @@ export class VuegularHelper {
     static setElementConfig<T> (config: T, target: any) {
         Reflect.defineMetadata(`vuegular:config`, config, target);        
     }
+
+    static getElementConfig (target: any): any {
+        Reflect.getMetadata(`vuegular:config`, target);        
+    }
 }
