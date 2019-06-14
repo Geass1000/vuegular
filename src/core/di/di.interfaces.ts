3ac1e5c7514e00db0dc2b8a866f505d3f43a8ac8
Andrey <geass1000@gmail.com>
2019-06-14 14:14:08 +0300

    add: DI interfaces

---
+++ src/core/di/di.interfaces.ts
@@ -0,0 +1,30 @@
+import { DIElementDataType } from './di.enums';
+import { DIToken } from './di-token';
+
+
+export namespace DIElement {
+    export type Key = DIToken | any;
+
+    export namespace Data {
+        export interface Class {
+            type: DIElementDataType.Class;
+            params: any[];
+        }
+        export interface Ref {
+            type: DIElementDataType.Ref;
+            key: Key;
+        }
+        export interface Value {
+            type: DIElementDataType.Value;
+            value: any;
+        }
+    }
+
+    export interface Options {
+        type: DIElementDataType;
+        multi: boolean;
+    }
+
+    export type Data = Data.Class | Data.Ref | Data.Value;
+}
+
