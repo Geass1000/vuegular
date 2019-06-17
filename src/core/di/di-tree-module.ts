eb28fbfa349be212b2a978a988d806ce5625ffcf
Andrey <geass1000@gmail.com>
2019-06-17 18:40:14 +0300

    add: list of DI Node Components

--- src/core/di/di-tree-module.ts
+++ src/core/di/di-tree-module.ts
@@ -3,7 +3,8 @@ import * as _ from 'lodash';
 import { DITreeProvider } from './di-tree-provider';
 
 export class DITreeModule<T, V> extends DITreeProvider<T, V> {
-    protected modules!: DITreeModule<T, V>[];
+    protected modules!: DITreeModule<any, any>[];
+    protected components!: DITreeProvider<any, any>[];
 
     constructor (protected type: T) {
         super(type);
@@ -13,10 +14,20 @@ export class DITreeModule<T, V> extends DITreeProvider<T, V> {
     /**
      * Sets the list of node of modules.
      * 
-     * @param  {any[]} modules
+     * @param  {DITreeModule<any,any>[]} modules
      * @returns void
      */
-    public setModules (modules: any[]): void {
+    public setModules (modules: DITreeModule<any, any>[]): void {
         this.modules = [ ...modules ];
+    }
+
+    /**
+     * Sets the list of node of components.
+     * 
+     * @param  {DITreeProvider<any,any>[]} components
+     * @returns void
+     */
+    public setComponents (components: DITreeProvider<any, any>[]): void {
+        this.components = [ ...components ];
     }
 }
