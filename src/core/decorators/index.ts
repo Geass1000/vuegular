bbfa3796940775e51d586ba513bd180f20c40767
Andrey <geass1000@gmail.com>
2019-06-18 16:23:12 +0300

    add: `Global Module` for reexporting

--- src/core/decorators/index.ts
+++ src/core/decorators/index.ts
@@ -1,4 +1,5 @@
 export * from './vg-module.decorator';
+export * from './vg-global-module.decorator';
 export * from './vg-component.decorator';
 export * from './vg-service.decorator';
 export * from './vg-inject.decorator';
